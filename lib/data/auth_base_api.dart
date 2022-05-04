d0b0e0a5699bc8c8c5c01b284ac0746848e2825e
Andruh Namolovan <andruh.namolovan@gmail.com>
2022-05-04 16:19:00 +0300

    - quality-check

---
+++ lib/data/auth_base_api.dart
@@ -0,0 +1,13 @@
+import 'package:movies_app/models/index.dart';
+
+abstract class AuthApiBase {
+  Future<AppUser> create({required String email, required String password, required String username});
+
+  Future<AppUser?> getCurrentUser();
+
+  Future<AppUser> login({required String email, required String password});
+
+  Future<void> logout();
+
+  Future<void> updateFavorites(String uid, int id, {required bool add});
+}
